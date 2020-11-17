6d642b892b769bbeac1d7c7f7a7e6cc09ab18389
Carolina Urrea <carolinaurrea@Carolinas-MacBook-Pro.local>
2020-11-17 14:02:20 -0500

    added photos

--- controllers/dinoController.js
+++ controllers/dinoController.js
@@ -9,6 +9,7 @@ dinoRouter.get('/', (req, res) => {
   res.render('dinosaurs/index', { dinos })
 })
 
+// new has to be above show, or else it will think that 'new' is an id value
 dinoRouter.get('/new', (req, res) => {
   res.render('dinosaurs/new')
 })
@@ -31,6 +32,24 @@ dinoRouter.post('/', (req, res) => {
   fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinos))
 
   res.redirect('/dinosaurs')
+})
+
+dinoRouter.get('/search/:searchTerm', (req, res) => {
+  const newDino = req.body
+  const rawDinos = fs.readFileSync('./dinosaurs.json')
+  const dinos = JSON.parse(rawDinos)
+  const searchTerm = req.params.searchTerm
+
+  // note that the details of the search are up to you!
+  // do partial matches count? do we look at the type property as well as the name property?
+  // this is a crude, 1st draft kind of search
+  const filteredDinos = dinos.filter((dino) => dino.name.toLowerCase() == searchTerm.toLowerCase())
+
+  console.log(searchTerm);
+  console.log(dinos);
+  console.log(filteredDinos);
+
+  res.render('dinosaurs/index', { dinos: filteredDinos })
 })
 
 module.exports = dinoRouter
